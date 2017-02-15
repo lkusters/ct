0c2347470dfeabfd44af295a5cca351e7f7c5b8a
Kusters <cjkusters@shark.researchlumc.nl>
2017-02-15 16:55:20 +0100

    adjust bins.R

--- bins.R
+++ bins.R
@@ -1,6 +1,6 @@
 args <- commandArgs(trailingOnly=TRUE)
 ##print("binning!")
-##file <- "/exports/lgtc/projects/kusters-Eexome/ct/out/div/divee1_1.txt.gz"
+##file <- "........./div/divee1_1.txt.gz"
 file <- args[1]
 data <- read.table(file) 
 ## use scan function ipv read, hoping that it will solve large file size issue
